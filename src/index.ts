339367d9ce5ff43d0130cb05ec6ce262f982e322
niroboo <116802762+niroboo@users.noreply.github.com>
2025-07-01 14:33:33 +0200

    Export query builder and utility types, so they can be used by projects depending on cosmonaut.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
+export type { BaseQueryBuilder, ConjunctionQueryBuilder, DisjunctionQueryBuilder } from './CosmosQueryBuilder';
+export type { ArrayElement, Path, PathValue, PickPath, UnionToIntersection } from './typeHelpers';
+
 export { CosmosQueryBuilder } from './CosmosQueryBuilder';
